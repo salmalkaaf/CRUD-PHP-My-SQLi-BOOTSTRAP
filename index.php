ccfa3dbb5989bf3451081ebae873a1f46e7ff476
salmalkaaf <sal.alkaaf@gmail.com>
2023-01-09 15:09:27 +0700

    CRUD_asset_form

--- index.php
+++ index.php
@@ -54,9 +54,14 @@
       </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+  
+     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+
+
+    <link rel="stylesheet" href="./assets/fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" href="./assets/css/style.css" />
+
     <script>
       $(document).ready( 
         function () {
